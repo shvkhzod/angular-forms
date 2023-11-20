d785c5a971d34d0b3d74ebf6f858934b70612171
Shakhzod Sharifov <thedigitaluzb@gmail.com>
2023-11-20 05:26:02 +0500

    test task for wiredhub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
   title = 'my-app';
+
+  email= new FormControl('', [Validators.email] )
+  password= new FormControl('', [Validators.minLength(8), Validators.maxLength(16)])
+
 }
